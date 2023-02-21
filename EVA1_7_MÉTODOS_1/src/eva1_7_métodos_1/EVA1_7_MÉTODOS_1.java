ab53995c636e742c5ab9cbc387fc098ad2d78a3d
Yessly Mayorga <106281825+YessPuentes12@users.noreply.github.com>
2023-02-21 09:35:33 -0600

    Add files via upload

---
+++ EVA1_7_MÉTODOS_1/src/eva1_7_métodos_1/EVA1_7_MÉTODOS_1.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package eva1_7_métodos_1;
+
+
+public class EVA1_7_MÉTODOS_1 {
+
+  
+    
+    public static void main(String[] args) {
+     myMethod(); //invocar al método, llamada a función.
+    }
+    
+   public static void myMethod(){
+   System.out.println("You have called me: my name is myMethod!!");
+   }
+    
+}
